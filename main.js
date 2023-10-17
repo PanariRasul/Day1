b7902eed517909df1e1b43a0df172a65c64651e5
PanariRasul <panarirasul@gmail.com>
2023-10-17 14:13:11 +0530

    Day1

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Wellcome JavaScript First Session");
+
+var a=10;
+console.log(a);
+
+var b = 20; 
+console.log(b);
+
+console.log(a + b);
+
+let Username = "Rasul01";
+let DOB = '2/4/2002';
+let Password = "Rasul@2002"
+
+console.log("Your Use Name: " + Username);
+console.log("Your Date Of Birth: " + DOB);
+console.log("Password: " + Password);
